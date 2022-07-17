e6a3bf22262cb30ad66f6d992d79cb0123fcd7ed
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2022-07-16 22:44:38 -0400

    Update primitiveToConventinal.py

--- primitiveToConventinal.py
+++ primitiveToConventinal.py
@@ -1,6 +1,6 @@
 from pymatgen.core import Structure
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
-str = Structure.from_file('FILENAME') # Replace FILENAME with your filename
+str = Structure.from_file('filename') # Replace filename with your structure file name
 sga = SpacegroupAnalyzer(str)
 conv = sga.get_conventional_standard_structure()
-conv.to(fmt='poscar', filename='conventional') # Replace PRMITIVE with your desired filename
+conv.to(fmt='poscar', filename='conventional') # Replace conventional with your desired conventional cell filename
