63a66070286624a1d369a444b9b58805d01a341f
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2022-07-16 22:45:30 -0400

    Update conventionalToPrimitive.py

--- conventionalToPrimitive.py
+++ conventionalToPrimitive.py
@@ -1,5 +1,5 @@
 from pymatgen.core import Structure
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
-str = Structure.from_file('FILENAME') # Replace FILENAME with your filename
+str = Structure.from_file('filename') # Replace filename with your structure file name
 strPrim = str.get_primitive_structure()
-strPrim.to(fmt='poscar', filename='PRIMITIVE') # Replace PRMITIVE with your desired filename
+strPrim.to(fmt='poscar', filename='primitive') # Replace primitive with your desired output filename
