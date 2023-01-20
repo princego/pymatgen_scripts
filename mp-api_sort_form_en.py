53d9423e93ca029c3420d805e01a4250cff3a7f6
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2023-01-20 23:22:16 +0530

    Update mp-api_sort_form_en.py

--- mp-api_sort_form_en.py
+++ mp-api_sort_form_en.py
@@ -1,17 +1,18 @@
 from mp_api.client import MPRester
 mpr = MPRester('<your api key>') # get it in materials project dashboard after signing up.
 
-# collecting mp ids for Hf-C based materials
-HfC_mp = mpr.get_materials_ids('Hf-C')
+# collecting mp ids
+structures = 'Hf-C' # here we get Hf-C based structures
+mp_ids = mpr.get_materials_ids(structures)
 
 # collecting formation energy per atom
-HfC_dict={}
-for i in range(len(HfC_mp)):
-    HfC_form_en = mpr.get_entry_by_material_id(HfC_mp[i].strip(), property_data=['formation_energy_per_atom'])[0].data['formation_energy_per_atom']
-    HfC_dict.update({HfC_mp[i].strip():HfC_form_en})
+structure_dict={}
+for i in range(len(mp_ids)):
+    form_en = mpr.get_entry_by_material_id(mp_ids[i].strip(), property_data=['formation_energy_per_atom'])[0].data['formation_energy_per_atom']
+    structure_dict.update({mp_ids[i].strip():form_en})
 
 # sorting according to least formation energy per atom
-sorted_dict = dict(sorted(HfC_dict.items(), key=lambda x:x[1]))
+sorted_dict = dict(sorted(structure_dict.items(), key=lambda x:x[1]))
 print(sorted_dict)
 
 # finding bulk and shear modulus for the lowest formation energy structure
