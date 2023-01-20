906b01820a57e435840e08e25a2a5d4f39d2b697
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2023-01-20 22:56:17 +0530

    Create mp_sort_form_en.py

---
+++ mp_sort_form_en.py
@@ -0,0 +1,12 @@
+# collecting mp ids
+HfC_mp = mpr.get_materials_ids('Hf-C')
+
+# collecting formation energy per atom
+HfC_dict={}
+for i in range(len(HfC_mp)):
+    HfC_form_en = mpr.get_entry_by_material_id(HfC_mp[i].strip(), property_data=['formation_energy_per_atom'])[0].data['formation_energy_per_atom']
+    HfC_dict.update({HfC_mp[i].strip():HfC_form_en})
+
+# sorting according to least formation energy per atom
+sorted_dict = dict(sorted(HfC_dict.items(), key=lambda x:x[1]))
+print(sorted_dict)
