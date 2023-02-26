c4e4b4a51b6b5c3296345387c09125f5d0b029f6
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2023-02-26 16:41:01 +0530

    Update vasp2cif_multiple.py

--- vasp2cif_multiple.py
+++ vasp2cif_multiple.py
@@ -8,14 +8,14 @@ import glob
 direc_vasp = os.getcwd()+'/data'    # keep all the .vasp files in a directory named data
 
 # output directory containing .cif files
-#os.mkdir('data_cif')   # creates a directory data_cif, where the cif files will the written
+os.mkdir('data_cif')   # creates a directory data_cif, where the cif files will the written
 direc_cif  = os.getcwd()+'/data_cif'
 
 os.chdir(direc_vasp)
-for poscar_file in glob.glob("*.vasp"):          # .vasp files in a directory 
-    struct_name = os.path.splitext(poscar_file)  # gives .vasp files
+for poscar_file in glob.glob("*.vasp"):          # searches for .vasp files in a directory 
+    struct_name = os.path.splitext(poscar_file)  
     struct_name_wo_ext = struct_name[0]          # structure name without extension
     struct = Structure.from_file(poscar_file)    # reads a poscar file using pymatgen
-    os.chdir(direc_cif)                          # change directory to direc_cif
-    struct.to(fmt='cif',filename=struct_name_wo_ext+'.cif') # saves the poscar file in cif format with cif extension
+    os.chdir(direc_cif)                          # change directory to direc_cif to write cif file
+    struct.to(fmt='cif',filename=struct_name_wo_ext+'.cif') # saves the poscar file with cif extension
     os.chdir(direc_vasp)                         # changes directory to direc_vasp
